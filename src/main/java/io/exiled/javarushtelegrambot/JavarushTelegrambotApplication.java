0e8ca195ccd81ef6cbc4dc1f359e9306b8560f92
ExiledRain <gett_lostt@bk.ru>
2021-03-10 19:44:05 +0200

    JRTB-0: added SpringBoot skeleton to the project.

---
+++ src/main/java/io/exiled/javarushtelegrambot/JavarushTelegrambotApplication.java
@@ -0,0 +1,13 @@
+package io.exiled.javarushtelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarushTelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavarushTelegrambotApplication.class, args);
+	}
+
+}
